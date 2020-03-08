2437b9ffd3abadf3e8e1c1a02560bfda18cc44a2
Salomo <hutapeasalomo@gmail.com>
2020-03-08 10:57:36 +0700

    Finished quiz app

---
+++ lib/result.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhase {
+    var resultText = 'You dit it!';
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likeable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are ... strange?!';
+    } else {
+      resultText = 'You are so bad!';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text('Restart Quiz!'),
+            textColor: Color.fromRGBO(200, 155, 233, 1),
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
+    );
+  }
+}
